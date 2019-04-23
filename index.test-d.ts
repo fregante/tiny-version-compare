cad964bf2fae22cb8a6f0f287ba4321a023a8177
Federico Brigante <github@bfred.it>
2019-04-23 17:47:57 +0800

    Lint

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,4 +1,4 @@
-import { expectType } from 'tsd';
+import {expectType} from 'tsd';
 import compareVersions from '.';
 
-expectType<number>(compareVersions("1.2.0", "2.3.0"));
+expectType<number>(compareVersions('1.2.0', '2.3.0'));
